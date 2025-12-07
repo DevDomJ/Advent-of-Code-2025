2d0ab9c93b7b6ebfcb253f2400ac724b1fcc546c
Dominik Jung <jung.dominik.dj@gmail.com>
2025-12-07 17:17:44 +0100

    Solved Riddle Day 6 - Part Two: Finally solved this fucking nightmare

--- RiddleSolutions/RiddleDay6.cs
+++ RiddleSolutions/RiddleDay6.cs
@@ -1,5 +1,6 @@
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Data.Common;
+using System.Runtime.CompilerServices;
+using System.Text;
+using Microsoft.VisualBasic;
 
 public class RiddleDay6
 {
@@ -28,7 +29,55 @@ public class RiddleDay6
 			homeWorkProblems.Add(new HomeWorkProblem(map[x]));
 		}
 
-		return (homeWorkProblems.Sum(problem => problem.Solve()), 0);
+		//Celaphoid solution requires a different approach due to their stupid way to save their data in the most inconvenient way possible.
+		var charMap = new CharMap(InputParser.GetInputString(inputPath));
+		//Find separator column indices first
+		var separatorIndices = new List<int>();
+		for (int x = charMap.HorizontalEdge; x >= 0; x--)
+		{
+			bool isSeparator = true;
+			for (int y = 0; y <= charMap.VerticalEdge; y++)
+			{
+				if (charMap.ElementAtPosition(x, y) != ' ')
+				{
+					isSeparator = false;
+					break;
+				}
+			}
+			if (isSeparator)
+			{
+				separatorIndices.Add(x);
+			}
+		}
+		var celapphoidHomeWorkProblems = new List<HomeWorkProblem>();
+		separatorIndices.Add(-1); //Add a fake separator at the end
+		var currentStartIndex = charMap.HorizontalEdge;
+		//Walk backwards to get columns
+		foreach (var separatorIndex in separatorIndices)
+		{
+			List<long> operands = new List<long>();
+			var currentOperator = '+';
+			for (int x = currentStartIndex; x > separatorIndex; x--)
+			{
+				var builder = new StringBuilder();
+				for (int y = 0; y <= charMap.VerticalEdge; y++)
+				{
+					var currentChar = charMap.ElementAtPosition(x, y);
+					if (currentChar == '+' || currentChar == '*')
+					{
+						currentOperator = currentChar;
+					}
+					else if (currentChar != ' ')
+					{
+						builder.Append(currentChar);
+					}
+				}
+				operands.Add(long.Parse(builder.ToString()));
+			}
+			celapphoidHomeWorkProblems.Add(new HomeWorkProblem(operands, currentOperator));
+			currentStartIndex = separatorIndex - 1;
+		}
+		return (homeWorkProblems.Sum(problem => problem.Solve()), celapphoidHomeWorkProblems.Sum(problem => problem.Solve()));
 	}
 
 }
@@ -43,6 +92,12 @@ public class HomeWorkProblem
 		homeWorkOperator = pieces.Last();
 		pieces.RemoveAt(pieces.Count - 1);
 		operands = pieces.Select(piece => long.Parse(piece));
+	}
+
+	public HomeWorkProblem(IEnumerable<long> operands, char homeWorkOperator)
+	{
+		this.operands = operands;
+		this.homeWorkOperator = homeWorkOperator.ToString();
 	}
 
 	public long Solve()
